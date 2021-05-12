95f03ab0cfbce85f2983426b0cc441ba5d4e0d21
m-tartari <michele.tartari@iit.it>
2021-05-12 14:17:01 +0200

    ✨ feat(test-2): add process_current_free_smooth.py

---
+++ utils/process_current_free_smooth.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import glob
+import yaml
+import numpy as np
+from matplotlib import pyplot as plt
+
+#import costum
+from utils import plot_utils
+
+def process(yaml_file, plot_all=False):
+    plt.rcParams['savefig.dpi'] = 300
+    repeat = 3
+    steps_1 = 13
+    steps_2 = 6
+
+    # read parameters from yaml file
+    print('[i] Using yaml_file: ' + yaml_file)
+    with open(yaml_file) as f:
+        try:
+            out_dict = yaml.safe_load(f)
+        except Exception:
+            raise Exception('error in yaml parsing')
+
+    # find logs
+    if 'location' in out_dict['log']:
+        head = out_dict['log']['location']
+    else:
+        head, _ =os.path.split(yaml_file)
+
+    if 'name' in out_dict['log']:
+        code_string = out_dict['log']['name']
+    else:
+        _, tail =os.path.split(yaml_file)
+        code_string = tail[:-len("-results.yaml")]
+
+    # set path to save graphs
+    if len(head)>6 and head[-6:]=='/logs/':
+        new_head = f'{head[:-6]}/images/'
+    else:
+        new_head = f'{head}/images/'
+
+    if os.path.isdir(new_head) is False:
+        try:
+            os.makedirs(new_head)
+        except OSError:
+            print("Creation of the directory %s failed" % new_head)
+    image_base_path= new_head +f'{code_string}_test-current-free-smooth'
+
+    log_file=head+f'{code_string}_test-current-free-smooth.log'
+    print('[i] Reading log_file: ' + log_file)
+
+    # log format: '%u64\t%u\t%u\t%u\t%u\t%f\t%f\t%d\t%f\t%f\t%f'
+    ns        = [np.uint64(x.split('\t')[0]) for x in open(log_file).readlines()]
+    motor_pos = [    float(x.split('\t')[1]) for x in open(log_file).readlines()]
+    motor_vel = [ np.int16(x.split('\t')[2]) for x in open(log_file).readlines()]
+    i_q       = [    float(x.split('\t')[3]) for x in open(log_file).readlines()]
+    i_fb      = [    float(x.split('\t')[4]) for x in open(log_file).readlines()]
+
+    if 'test_current' in out_dict:
+        number_of_iters = out_dict['test_current']['number_of_iters']
+        rise_time = out_dict['test_current']['rise_time']
+        wait_time = out_dict['test_current']['wait_time']
+        t_steps = [ns[0]/1e9 + rise_time]
+        t_steps.append(t_steps[-1]+wait_time)
+        t_steps.append(t_steps[-1]+rise_time)
+        t_steps.append(t_steps[-1]+wait_time)
+        if number_of_iters > 1:
+            for n in range(1,number_of_iters):
+                t_steps.append(t_steps[-1]+rise_time)
+                t_steps.append(t_steps[-1]+wait_time)
+                t_steps.append(t_steps[-1]+rise_time)
+                t_steps.append(t_steps[-1]+wait_time)
+    else:
+        raise Exception("missing 'test_current' in yaml parsing")
+
+    print(f'[i] Processing data (loaded {len(ns)} points)')
+
+    # motor_vel vs time ------------------------------------------------------
+    fig2, axs = plt.subplots()
+    #l0 = axs.plot([s/1e9 for s in ns], i_fb, color='#8e8e8e', marker='.', markersize=0.2, linestyle="", label='current out fb (A)')
+    l1 = axs.plot([s/1e9 for s in ns], i_q, color='#1e1e1e', marker='.', markersize=0.2, linestyle="-", label='current reference (A)')
+    l2 = axs.plot([s/1e9 for s in ns], i_fb, color='#2ca02c', marker='.', markersize=0.2, linestyle=":", label='current ref fb (A)')
+    l3 = axs.plot([s/1e9 for s in ns], [float(v)/1e3 for v in motor_vel], color='#1f77b4', marker='.', markersize=0.2, label='motor velocity (rad/s)')
+    axs.legend()
+    for l in t_steps:
+        axs.axvline(l, linestyle='--', color='r', alpha=0.5, zorder=1)
+
+    # make plot pretty
+    axs.set_xlabel('Time (s)')
+    axs.set_xlim(ns[0]/1e9, t_steps[-1])
+    plt_max = (max(i_q) -min(i_q)) * 0.05
+    axs.set_ylim(min(i_q)-plt_max, max(i_q)+plt_max)
+    axs.grid(b=True, which='major', axis='x', linestyle=':')
+    axs.grid(b=True, which='major', axis='y', linestyle='-')
+    axs.grid(b=True, which='minor', axis='y', linestyle=':')
+    axs.spines['top'].set_visible(False)
+    axs.spines['right'].set_visible(False)
+
+    # Save the graph
+    fig_name = image_base_path + '_1.png'
+    print('Saving graph as: ' + fig_name)
+    plt.savefig(fname=fig_name, format='png', bbox_inches='tight')
+    if plot_all:
+        plt.show()
+
+    # motor_vel vs i_q ------------------------------------------------------
+    fig, axs = plt.subplots()
+    # l0 = axs.plot(i_fb,  motor_vel, color='#8e8e8e', marker='.', markersize=0.2, linestyle="", label='current out fb (A)')
+    l1 = axs.plot(i_q, motor_vel, color='#1e1e1e', marker='.', markersize=0.2, linestyle="-", label='current reference (A)')
+    l2 = axs.plot(i_fb,  motor_vel, color='#2ca02c', marker='.', markersize=0.2, linestyle=":", label='current ref fb (A)')
+    axs.legend()
+
+    # make plot pretty
+    axs.set_xlabel('Current (A)')
+    axs.set_ylabel('motor velocity (mrad/s)')
+    plt_max = (max(i_q) -min(i_q)) * 0.05
+    axs.set_xlim(min(i_q)-plt_max, max(i_q)+plt_max)
+    axs.grid(b=True, which='major', axis='x', linestyle=':')
+    axs.grid(b=True, which='major', axis='y', linestyle='-')
+    axs.grid(b=True, which='minor', axis='y', linestyle=':')
+    axs.spines['top'].set_visible(False)
+    axs.spines['right'].set_visible(False)
+
+    # Save the graph
+    fig_name = image_base_path + '_2.png'
+    print('Saving graph as: ' + fig_name)
+    plt.savefig(fname=fig_name, format='png', bbox_inches='tight')
+    if plot_all:
+        plt.show()
+
+    return yaml_file
+
+if __name__ == "__main__":
+    plot_utils.print_alberobotics()
+    print(plot_utils.bcolors.OKBLUE + "[i] Starting process_current_free_smooth" + plot_utils.bcolors.ENDC)
+    yaml_file = process(yaml_file=sys.argv[1], plot_all=False)
+
+    print(plot_utils.bcolors.OKGREEN + u'[\u2713] Ending program successfully' + plot_utils.bcolors.ENDC)
